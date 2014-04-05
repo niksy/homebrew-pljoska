67b7a12a46d24a442f8aa169743500f5954ceef0
Ivan Nikolić <niksy5@gmail.com>
2014-04-05 09:46:26 +0200

    Update SHA1 for flegma

--- Formula/flegma.rb
+++ Formula/flegma.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Flegma < Formula
   homepage 'https://github.com/niksy/flegma'
   url 'https://github.com/niksy/flegma/archive/0.1.0.tar.gz'
-  sha1 '235b253eb923226ea98933c3a085c6b1998f7a8a'
+  sha1 '7c9be8b2711efd51caeba880ebaa740e1d4f491a'
 
   depends_on 'speedtest_cli' => :recommended
 

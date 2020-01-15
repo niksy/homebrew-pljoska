659dfe7e6200d258bff571edd962060868e673d6
Ivan Nikolić <niksy5@gmail.com>
2020-01-15 08:58:43 +0100

    Update rtunic binary file

--- Casks/rtunic.rb
+++ Casks/rtunic.rb
@@ -2,7 +2,7 @@ cask 'rtunic' do
   version '1.0.20'
   sha256 :no_check
 
-  url "https://realtekcdn.akamaized.net/rtdrivers/cn/nic1/RTUNICv1.0.20_Update2.zip?__token__=exp=1~acl=/rtdrivers/cn/nic1/RTUNICv1.0.20_Update2.zip~hmac=c8b02123a9ce15e912fb79b1f4b75ac81e438b94a9101ff4184f0a7a57b7e1e4"
+  url "https://github.com/niksy/homebrew-pljoska/raw/master/downloads/RTUNICv1.0.20_Update2.zip"
   name 'Realtek USB hub drivers'
   homepage 'https://www.realtek.com/en/component/zoo/category/network-interface-controllers-10-100-1000m-gigabit-ethernet-usb-3-0-software'
 

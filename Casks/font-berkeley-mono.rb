f11a88bb1a2ca41167c8f69cfc0761af0542a615
Ivan Nikolić <niksy5@gmail.com>
2023-02-13 16:39:27 +0100

    Add cask for Berkeley Mono

--- Casks/font-berkeley-mono.rb
+++ Casks/font-berkeley-mono.rb
@@ -2,7 +2,7 @@ cask 'font-berkeley-mono' do
 
   version :latest
   sha256 :no_check
-  url ENV['BERKELEY_MONO_DOWNLOAD_URL']
+  url "https://drive.google.com/uc?id=#{ENV['BERKELEY_MONO_DOWNLOAD_URL']}&export=download"
   homepage 'https://berkeleygraphics.com/typefaces/berkeley-mono/'
 
   font 'BerkeleyMonoTrial-Regular.ttf'

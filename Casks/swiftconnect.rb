32f2639e69b443e512a9c26ae504c84b506a0b7c
Ivan Nikolić <niksy5@gmail.com>
2023-04-05 14:36:11 +0200

    Add OpenConnect clients

--- Casks/swiftconnect.rb
+++ Casks/swiftconnect.rb
@@ -4,7 +4,7 @@ cask "swiftconnect" do
 
   sha256 "8601dfc9b52434dd7f373103b678d85d68c93ed53da9f7902bfdee165472429f"
 
-  url "https://github.com/wenyuzhao/SwiftConnect/releases/download/v#{version}}/SwiftConnect.app.zip",
+  url "https://github.com/wenyuzhao/SwiftConnect/releases/download/v#{version}/SwiftConnect.app.zip",
       verified: "github.com/wenyuzhao/SwiftConnect"
 
   name "SwiftConnect"
